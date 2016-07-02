6d286bbff12f5e0a1fc2fce8388513ea3768cea3
asimonia <alex.simonian@gmail.com>
2016-07-02 17:53:48 -0500

    Video list items updated

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ class App extends Component {
 
 		this.state = { videos: [] };
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+		YTSearch({key: API_KEY, term: 'beestings'}, (videos) => {
 			this.setState({ videos });
 		});
 	}
@@ -31,24 +31,3 @@ class App extends Component {
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
-
-
-/*
-React code --> components/view
-Something that barfs out HTML
-
-We write code and nest components
-
-A component is a collection of JS components that ultimately produces HTML
-*/
-
-// Create a new component.  This component should produce HTML
-// const declares a variable. A constant variable.
-// jsx is a dialect of javascript (looks like HTML)
-// webpack and babel transpile the code
-// The JSX -> HTML -> documentOM
-// JSX -> jav
-// React 		--> to create the component
-// ReactDom		--> to render to the DOM
-
-// One component for fileascript HTML
